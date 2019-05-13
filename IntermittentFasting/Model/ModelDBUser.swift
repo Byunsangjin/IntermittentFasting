13aa91386d43ff17fdffa00fe97bb8d693874cc3
byunsangjin <qustkdwls1@naver.com>
2019-05-13 12:56:26 +0900

    병합

---
+++ IntermittentFasting/Model/ModelDBUser.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  IntermittentFasting
+//
+//  Created by Byunsangjin on 10/05/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+import Foundation
---
+++ IntermittentFasting/Model/ModelWeight.swift
@@ -0,0 +1,9 @@
+//
+//  ModelWeight.swift
+//  IntermittentFasting
+//
+//  Created by Byunsangjin on 10/05/2019.
+//  Copyright © 2019 Byunsangjin. All rights reserved.
+//
+
+import Foundation
